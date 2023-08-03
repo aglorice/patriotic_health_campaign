3013e3ed7aa2ad1434c3cc47d10ac0054a187a22
aglorice <401208941@qq.com>
2023-08-03 14:20:12 +0800

    feat:新增路由组件

---
+++ src/components/navigation_bar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/activity_introduction">ActivityIntroduction</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navigation_bar"
+}
+</script>
+
+<style scoped>
+
+</style>
