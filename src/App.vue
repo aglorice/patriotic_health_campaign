f1c2e273a0d40dc78646666b196480eb86340d33
aglorice <401208941@qq.com>
2023-08-03 14:18:46 +0800

    feat:新增切换路由动画

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
-	<div>
-		<home_page></home_page>
+	<div id="app">
+<!--    <home_page></home_page>-->
+    <transition :name="transitionName" mode="out-in">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </transition>
 	</div>
 </template>
 
 <script>
-import home_page from "@/components/home_page.vue";
+// import home_page from "@/pages/home/home_page.vue";
 
 export default {
+
 	name: 'App',
-	components: {home_page}
+  data(){
+    return{
+      transitionName:'slide-left'
+    }
+  },
+	// components: {home_page}
 }
 </script>
 
