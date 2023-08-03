114dfe6fdb6635671d2b4c887af8821315849886
qiguai <3124140355@qq.com>
2023-08-03 12:10:33 +0800

    feat:新增首页组件

---
+++ src/components/home_page.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+	<router-link to="/activity_introduction">活动介绍</router-link>
+	<router-link to="/activity_introduction">活动宣传</router-link>
+	<router-link to="/activity_introduction">互动交流</router-link>
+	<router-link to="/activity_introduction">成果展示</router-link>
+</div>
+</template>
+
+<script>
+export default {
+	name: 'HomePage'
+}
+</script>
+
+<style scoped>
+a {
+	margin: 5px;
+}
+</style>
