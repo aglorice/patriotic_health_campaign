d2c042e29d1ab10006adca60061b61912a1452f5
aglorice <401208941@qq.com>
2023-08-03 14:19:26 +0800

    update:将导航抽出作为一个导航栏组件✨

---
+++ src/pages/home/home_page.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <h1>HomePage</h1>
+  <navigation_bar></navigation_bar>
+</div>
+</template>
+
+<script>
+import navigation_bar from "@/components/navigation_bar";
+
+export default {
+	name: 'HomePage',
+  components: {navigation_bar}
+}
+</script>
+
+<style scoped>
+a {
+	margin: 5px;
+}
+</style>
