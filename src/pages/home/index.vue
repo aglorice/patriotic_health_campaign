524c80d7b5fab80bad18ea42e121a2a71f396db5
aglorice <401208941@qq.com>
2023-08-04 16:27:29 +0800

    feat:👀👀

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,23 +1,37 @@
 <template>
 	<div>
 		<NavigationBar/>
-		<div class="test"></div>
+		<div class="test">
+			<CarouselImg :images="images" />
+		</div>
+		<div>
+			<h1>我是首页</h1>
+		</div>
 	</div>
 </template>
 
 <script>
 import NavigationBar from "@/components/navigation_bar";
-
+import CarouselImg from "@/components/carousel_img";
 export default {
 	name: 'HomePage',
+	data(){
+		return{
+			images:[
+					"https://aglorice.cn/img/index.97db8b1b.png",
+					"https://aglorice.cn/img/index.97db8b1b.png",
+			]
+		}
+	},
 	components: {
 		NavigationBar,
+		CarouselImg
 	}
 }
 </script>
 
 <style scoped>
 .test {
-	height: 10000px;
+
 }
 </style>
