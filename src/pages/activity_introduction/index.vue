e8bf7735c2f04d860890aed050569fce7c782ea6
aglorice <401208941@qq.com>
2023-08-04 21:01:01 +0800

    feat:新增时间线

--- src/pages/activity_introduction/index.vue
+++ src/pages/activity_introduction/index.vue
@@ -1,10 +1,99 @@
 <template>
-	<div class="swiper_top">
+	<div class="app">
+		<NavigationBar/>
+		<div class="introduction">
+			<div class="content">
+				<p class="content_p">活动简介</p>
+				<span>
+					爱国卫生运动是指强化全民健康意识，养成文明卫生习惯，改善卫生条件，减少健康危害因素，提高公民健康水平的社会性、群众性卫生活动。
+				</span>
+			</div>
+		</div>
+		<div class="mid_content">
+			<div >
+				<p>活动介绍</p>
+			</div>
+			<div class="time_line">
+				<div class="time_line_top">
+					<h1>发展历史</h1>
+					<p>
+						早在第二次国内革命战争时期，党就把组织军民开展群众卫生运动，搞好卫生防病工作，当作关系革命成败的一件大事来抓。1933年，毛泽东同志在《长冈乡调查》一文中指出：“疾病是苏区中一大仇敌，因为它减弱我们的力量。如长冈乡一样，发动广大群众的卫生运动，减少疾病以至消灭疾病，是每个乡苏维埃的责任”。
+					</p>
+					<p>
+						在抗日战争和解放战争时期，陕甘宁边区政府把开展全地区卫生运动列为施政纲领。1941年陕甘宁边区成立了防疫委员会，开展以灭蝇、灭鼠，防止鼠疫、霍乱为中心的军民卫生运动。
+					</p>
+				</div>
 
+				<div class="entries">
+					<div class="entry">
+						<div class="title">1949年</div>
+						<div class="body">
+							<p>
+								1949年，贯彻预防为主卫生工作方针，继续开展群众性卫生运动。1952年春，美帝国主义在侵朝战争中，对朝鲜和我国发动了细菌战争。在保家卫国的浪潮中，推动了群众性卫生防疫运动的深入发展。人民群众把这项伟大的运动称之为“爱国卫生运动”。党中央肯定了这个名称并指示各级领导机构，以后统称为“爱国卫生运动委员会”。在此同时，把“卫生工作与群众性卫生运动相结合”定为卫生工作的一项原则。毛泽东同志号召：“动员起来，讲究卫生，减少疾病，提高健康水平，粉碎敌人的细菌战争”。爱国卫生运动不仅受到全国上下的一致拥护和参与，而且受到国际上的赞誉。在社会主义革命和社会主义建设的各个历史时期，显示出它的“移风易俗，改造国家”的伟大作用，取得了丰硕的成果。
+							</p>
+						</div>
+					</div>
+					<div class="entry">
+						<div class="title">1952年</div>
+						<div class="body">
+							<p>
+								在1949-1952年，即国民经济恢复期间，为了改变旧中国不卫生状况和传染病严重流行的现实，在全国普遍开展了群众性卫生运动。在抗美援朝，粉碎美帝细菌战争期间，在中央防疫委员会的领导下，各地迅速掀起了群众性卫生运动的新高潮，即“除四害运动”。运动规模之大，参加人数之多，收效之显著，都是空前的。仅半年里，全国就清除垃圾1500多万吨，疏通渠道28万公里，新建改建厕所490万个，改建水井130万眼。共扑鼠4400多万只，消灭蚊、蝇、蚤共200多万斤。还填平了一大批污水坑塘；广大城乡的卫生面貌有了不同程度的改善。
+							</p>
+						</div>
+					</div>
+					<div class="entry">
+						<div class="title big">1966年</div>
+						<div class="body">
+							<p>
+								在1953-1966年，即社会主义改造和建设时期，当时政务院发出继续开展爱国卫生运动的指示，要求着重抓好城市厂矿，并把突击活动与经常保洁结合起来。随着工作的开展，不少厂矿建立了清洁队，卫生扫除日等各种制度；保持车间、居室、食堂、厕所的内、外环境清洁整齐，从而使职工患病率逐渐下降，健康水平有所提高，促进了生产任务的发展。
+								1955年冬，毛泽东同志起草的一个通知说：把爱国卫生运动和“除四害”讲卫生结合起来，对防治人畜疾病，保障健康起到很好的作用。1956年1月12日，《人民日报》发表了题为《除四害》的社论，号召全国人民行动起来消灭老鼠、麻雀、苍蝇、蚊子。将麻雀定为“害鸟”，是爱国卫生运动中走过的一段弯路，不过“除四害”活动对传染病和流行病的防治，起到了积极有效的作用。
+								1960年3月，全国人大通过的《1956-1967年全国农业发展纲要》把“除四害”讲卫生列入“纲要”的内容。在党中央及各级政府领导下，动员广大群众积极参加，同样收到立竿见影的效果。许多城乡清除了大量的垃圾、污物.
+							</p>
+						</div>
+					</div>
+					<div class="entry">
+						<div class="title">1976年</div>
+						<div class="body">
+							<p>
+								1966-1976年，“文化大革命”时期，许多卫生防保，包括各级爱国卫生运动委员会和其办事机构被撤并，人员有的被裁减，有的被改行；有些工作被迫处于停顿状态，致使许多城乡的卫生面貌开始恶化，疫情回升。此种现象反映到党中央，当时，周恩来同志多次指示，要继续开展爱国卫生运动，并亲自组织指导防疫队和支援西北地区的巡回医疗队，控制传染病流行，改善缺医少药和不卫生的状况。这个时期，各地卫生防疫专业人员和爱国卫生运动工作者，深入实际，进行卫生科学知识宣传，努力改善环境卫生面貌。在农村，解决好管水和粪这两个基本问题，改造环境卫生条件所采取的各项措施受到农民群众的欢迎，并概括为“两管、五改”，即管水、管粪，改水井、改厕所、改畜圈、改炉灶、改造环境。“两管、五改”已成为组织指导农村爱国卫生运动的具体要求和行动目标。
+							</p>
+						</div>
+					</div>
+					<div class="entry">
+						<div class="title">1995年</div>
+						<div class="body">
+							<p>
+								1978-1995年，我国进入了改革开放时期。党的十一届三中全会以来，爱国卫生运动进入了一个新的历史时期。1978年4月，国务院发出《关于坚持开展爱国卫生运动的通知》，要求各地爱国卫生运动委员会及其办事机构，把卫生运动切实领导起来.
+							</p>
+						</div>
+					</div>
+					<div class="entry">
+						<div class="title big">2015年</div>
+						<div class="body">
+							<p>1月13日电 国务院近日印发《关于进一步加强新时期爱国卫生工作的意见》（以下简称《意见》），就做好新形势下的爱国卫生工作提出明确要求。这是国务院时隔25年又一次专题印发指导开展爱国卫生工作的重要文件。</p>
+						</div>
+					</div>
+					<div class="entry">
+						<div class="title">2020年</div>
+						<div class="body">
+							<p>2020年3月2日习近平总书记在北京考察新冠肺炎防控科研攻关工作时强调：“坚持开展爱国卫生运动”；在浙江考察时强调：“要深入开展爱国卫生运动，推进城乡环境整治，完善公共卫生设施，提倡文明健康、绿色环保的生活方式”。爱国卫生运动是中国人民的一项伟大创举，是确保人民群众生命安全和身体健康的传家宝。新时代，我们要继续用好这一传家宝，把爱国卫生运动提高到新水平。</p>
+						</div>
+					</div>
+					<div class="entry">
+						<div class="title">2022年</div>
+						<div class="body">
+							<p>2022年是爱国卫生运动70周年。爱国卫生运动不是简单的清扫卫生，更多应该从人居环境改善、饮食习惯、社会心理健康、公共卫生设施等多个方面开展工作，提倡文明健康、绿色环保的生活方式。 [5]
+								爱国卫生运动是中国共产党把群众路线运用于卫生防病工作的伟大创举和成功实践，创造了卫生与健康的“中国奇迹”。</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
+import NavigationBar from "@/components/navigation_bar";
 export default {
 	name: 'ActivityIntroduction',
 	data() {
@@ -12,21 +101,135 @@ export default {
 
 		}
 	},
-	methods: {
-		onSwiper() {
-
-		},
-		onSlideChange() {
-		}
+	components:{
+		NavigationBar
 	}
 }
 </script>
 
 <style scoped>
-/*轮播图顶部*/
-.swiper_top {
-	width: 100vw;
-	height: 100vh;
+/*顶部大图*/
+.app {
+	top: 0;
+}
+.introduction {
+	position: relative;
+	width: 100%;
+	height: 80vh;
+	color: white;
+	/* 背景图不平铺 */
+	/* 当内容高度大于图片高度时，背景图像的位置相对于viewport固定 */
+	background: url("@/assets/background/1.png") no-repeat fixed top center;
+	/* 让背景图基于容器大小伸缩 */
+	background-size: cover;
+}
+.content {
+	position: absolute;
+	top: 75%;
+	left: 100px;
+	right: 50%;
+	transform: translateY(-50%);
+}
+.content span {
+	letter-spacing: 0.2em;
+	text-indent: 2em;
+}
+
+.content_p {
+	font-weight: lighter;
+	letter-spacing: 0.3em;
+	font-size: 2.4em;
+	margin-bottom: 20px;
+	padding-bottom: 10px;
+	padding-left: 10px;
+	border-bottom: 2px solid #ffffff;
+	border-left: 15px solid #f67373;
+}
+.time_line {
+	background-color: #b0b0b0;
+	min-height: 100vh;
+	margin: 0;
+	font-family: "Droid Sans", sans-serif;
+}
+.time_line h1 {
+	text-align: center;
+	letter-spacing: 0.3em;
+	font-size: 2.4em;
+	line-height: 2.4em;
+}
+.time_line_top p {
+	text-align: center;
+	font-size: 0.9em;
+	margin: 10px 80px 40px 80px;
+	font-weight: lighter;
+	text-indent: 2em;
+}
+/*.time_line:before {*/
+/*	content: "";*/
+/*	position: absolute;*/
+/*	top: 60vh;*/
+/*	left: 50%;*/
+/*	bottom: 0px;*/
+/*	transform: translateX(-50%);*/
+/*	width: 4px;*/
+/*	background-color: #f50404;*/
+/*}*/
+.time_line .entries {
+	width: calc(100% - 80px);
+	max-width: 800px;
+	margin: auto;
+	position: relative;
+	left: -5px;
+}
+.time_line .entries .entry {
+	width: calc(50% - 80px);
+	float: left;
+	padding: 20px;
+	clear: both;
+	text-align: right;
+}
+.time_line .entries .entry:not(:first-child) {
+	margin-top: -60px;
+}
+.time_line .entries .entry .title {
+	font-size: 32px;
+	margin-bottom: 12px;
+	position: relative;
+	color: #000000;
+}
+.time_line .entries .entry .title:before {
+	content: "";
+	position: absolute;
+	width: 8px;
+	height: 8px;
+	border: 4px solid #ffffff;
+	background-color: #f37070;
+	border-radius: 100%;
+	top: 50%;
+	transform: translateY(-50%);
+	right: -73px;
+	z-index: 1000;
+}
+.time_line .entries .entry .title.big:before {
+	width: 24px;
+	height: 24px;
+	transform: translate(8px, -50%);
+}
+.time_line .entries .entry .body {
+	color: #000000;
+}
+.time_line .entries .entry .body p {
+	line-height: 1.4em;
+}
+.time_line .entries .entry:nth-child(2n) {
+	text-align: left;
+	float: right;
+}
+.time_line .entries .entry:nth-child(2n) .title:before {
+	left: -63px;
+}
+.time_line .entries .entry:nth-child(2n) .title.big:before {
+	transform: translate(-8px, -50%);
 }
 
 </style>
