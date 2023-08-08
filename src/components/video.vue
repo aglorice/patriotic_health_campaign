fbccb39acc11d2197bdbf2d90db41bb9af896601
qiguai <3124140355@qq.com>
2023-08-08 21:42:43 +0800

    feat:新增视频组件

---
+++ src/components/video.vue
@@ -0,0 +1,27 @@
+<template>
+	<div ref="video" class="video"></div>
+</template>
+
+<script>
+import DPlayer from "dplayer";
+
+export default {
+	name: 'VideoPage',
+	props: ['videoSrc'],
+	mounted() {
+		new DPlayer({
+			container: this.$refs.video,
+			video: {
+				url: this.videoSrc
+			},
+		});
+	}
+}
+</script>
+
+<style scoped>
+.video {
+	width: 100%;
+	height: 100%;
+}
+</style>
