6b1c32a0aa345cff730a4517be5e9dc4c60ae1ca
aglorice <401208941@qq.com>
2023-08-08 19:10:37 +0800

    feat:完善成果展示页面🚀

--- src/pages/results_show/index.vue
+++ src/pages/results_show/index.vue
@@ -1,17 +1,217 @@
 <template>
 	<div>
-		<h1>results_show</h1>
+		<div class="swiper_home">
+			<CarouselImg :images="images"/>
+		</div>
+
+		<div class="mid_box">
+			<div class="mid_box_1">
+				<div class="box_title">
+					<h1>成果展示</h1>
+					<p>
+						爱国卫生运动最早可追溯至1952年，当年，为防御细菌战，毛泽东同志题词指出：“动员起来，讲究卫生，减少疾病，提高健康水平，粉碎敌人的细菌战争”，在全国倡导发起轰轰烈烈的爱国卫生运动。同年，中央成立防疫委员会，随后更名为中央爱国卫生运动委员会，周恩来同志担任首届中央爱国卫生运动委员会主任。从那时起，爱国卫生运动就在全国上下蓬勃开展，围绕解决不同时期人民群众生产生活中的突出卫生问题，先后开展了灭蚊、灭蝇、灭鼠和灭蟑螂为重点的“除四害”、“两改五改”、“五讲四美三热爱”、城乡环境卫生整洁行动、卫生城市（镇）创建、健康城市（镇）建设等一系列工作，爱国卫生工作从城市向乡村辐射，城乡人居环境取得显著成效。</p>
+				</div>
+				<DividerComponent content-position="center">主要成果</DividerComponent>
+				<div class="show_box">
+					<div class="item">
+						<div class="item_img">
+							<img src="@/assets/result_show/887.jpg" alt="">
+						</div>
+						<div class="item_box">
+							<div class="box_item_title">
+								<h1>除四害</h1>
+								<p>
+									在党中央领导下，全国范围内迅速掀起了剿灭“四害”运动的高潮，全国6亿人民参与其中，有效降低了病媒生物密度，显著减少了鼠疫、疟疾、流行性出血热等疾病的发生和流行，城乡面貌焕然一新。在五十年代，韶关地区积极组织开展了群众性卫生运动。在历史上，当时的韶关地区乐昌县岐乐村成为全国一面红旗，成为了全国农村爱卫工作学习的榜样！</p>
+								<p></p>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="item_img">
+							<img src="@/assets/result_show/887.jpg" alt="">
+						</div>
+						<div class="item_box">
+							<div class="box_item_title">
+								<h1>除四害</h1>
+								<p>在党中央领导下，全国范围内迅速掀起了剿灭“四害”运动的高潮，全国6亿人民参与其中，有效降低了病媒生物密度，显著减少了鼠疫、疟疾、流行性出血热等疾病的发生和流行，城乡面貌焕然一新。在五十年代，韶关地区积极组织开展了群众性卫生运动。在历史上，当时的韶关地区乐昌县岐乐村成为全国一面红旗，成为了全国农村爱卫工作学习的榜样！</p>
+								<p></p>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="item_img">
+							<img src="@/assets/result_show/887.jpg" alt="">
+						</div>
+						<div class="item_box">
+							<div class="box_item_title">
+								<h1>除四害</h1>
+								<p>在党中央领导下，全国范围内迅速掀起了剿灭“四害”运动的高潮，全国6亿人民参与其中，有效降低了病媒生物密度，显著减少了鼠疫、疟疾、流行性出血热等疾病的发生和流行，城乡面貌焕然一新。在五十年代，韶关地区积极组织开展了群众性卫生运动。在历史上，当时的韶关地区乐昌县岐乐村成为全国一面红旗，成为了全国农村爱卫工作学习的榜样！</p>
+								<p></p>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="item_img">
+							<img src="@/assets/result_show/887.jpg" alt="">
+						</div>
+						<div class="item_box">
+							<div class="box_item_title">
+								<h1>除四害</h1>
+								<p>在党中央领导下，全国范围内迅速掀起了剿灭“四害”运动的高潮，全国6亿人民参与其中，有效降低了病媒生物密度，显著减少了鼠疫、疟疾、流行性出血热等疾病的发生和流行，城乡面貌焕然一新。在五十年代，韶关地区积极组织开展了群众性卫生运动。在历史上，当时的韶关地区乐昌县岐乐村成为全国一面红旗，成为了全国农村爱卫工作学习的榜样！</p>
+								<p></p>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="item_img">
+							<img src="@/assets/result_show/887.jpg" alt="">
+						</div>
+						<div class="item_box">
+							<div class="box_item_title">
+								<h1>除四害</h1>
+								<p>在党中央领导下，全国范围内迅速掀起了剿灭“四害”运动的高潮，全国6亿人民参与其中，有效降低了病媒生物密度，显著减少了鼠疫、疟疾、流行性出血热等疾病的发生和流行，城乡面貌焕然一新。在五十年代，韶关地区积极组织开展了群众性卫生运动。在历史上，当时的韶关地区乐昌县岐乐村成为全国一面红旗，成为了全国农村爱卫工作学习的榜样！</p>
+								<p></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="">
+
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
+import CarouselImg from "@/components/carousel_img";
+import DividerComponent from "@/components/divider";
+
 export default {
 	name: "ResultsShow",
+	data() {
+		return {
+			images: [
+				require("@/assets/result_show/881.jpg"),
+				require("@/assets/result_show/887.jpg"),
+				require("@/assets/result_show/908.jpg"),
+				require("@/assets/result_show/910.jpg"),
+			]
+		}
+	},
+	components: {
+		DividerComponent,
+		CarouselImg
+	},
 }
 </script>
 
 <style scoped>
-h1 {
-	margin-top: 100px;
+.swiper_home {
+	width: 100%;
+	height: 60vh;
 }
+
+.mid_box {
+	width: 100%;
+}
+
+.mid_box_1 {
+	max-width: 85%;
+	margin: 0 auto;
+}
+
+.box_title {
+	text-align: center;
+	margin: 60px 0 60px 0;
+}
+
+.box_title h1 {
+	margin: 0;
+	font-size: 2.2em;
+	letter-spacing: 0.2em;
+	font-weight: lighter;
+}
+
+.box_title h1::after {
+	content: "";
+	display: block;
+	width: 260px;
+	height: 2px;
+	background-color: #810404;
+	margin: 20px auto 0 auto;
+}
+
+.box_title p {
+	margin: 20px 0 0 0;
+	text-indent: 2em;
+	letter-spacing: 0.1em;
+	font-size: 0.9em;
+	line-height: 2em;
+}
+
+.show_box {
+	margin: 60px 0 0 0;
+}
+
+.item {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	/*padding: 10vh 20px 10vh 20px;*/
+	box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+	margin-bottom: 60px;
+}
+
+.item_img {
+	height: 100%;
+	flex: 5;
+}
+
+.item:nth-child(odd) {
+	flex-direction: row;
+}
+
+.item:nth-child(even) {
+	flex-direction: row-reverse;
+}
+
+.item_img img {
+	width: 100%;
+	display: block;
+}
+
+.item_box {
+	flex: 5;
+}
+
+.box_item_title {
+	padding: 30px;
+	text-align: center;
+}
+
+.box_item_title h1 {
+	font-size: 2em;
+	margin: 0;
+	letter-spacing: 0.2em;
+	font-weight: normal;
+}
+
+.box_item_title h1::after {
+	content: "";
+	display: block;
+	width: 60px;
+	height: 2px;
+	background-color: #810404;
+	margin: 20px auto 0 auto;
+}
+
+.box_item_title p {
+	margin: 20px 0 0 0;
+	text-indent: 2em;
+	letter-spacing: 0.1em;
+	font-size: 0.9em;
+	line-height: 2em;
+}
+
 </style>
