19cb1945a6b97e5974223df33bb20395314d2993
qiguai <3124140355@qq.com>
2023-08-09 21:43:47 +0800

    test:对投票组件的测试

--- src/pages/interaction/index.vue
+++ src/pages/interaction/index.vue
@@ -1,17 +1,29 @@
 <template>
-	<div>
-		<h1>interaction</h1>
+	<div class="vote">
+		<Vote :data="voteData" />
 	</div>
 </template>
 
 <script>
+import Vote from "@/components/vote.vue";
+
 export default {
 	name: "InteractionPage",
+	components: {Vote},
+	data() {
+		return {
+			voteData: {
+				name: ['方案1', '方案2', '方案3', '方案4', '方案5'],
+				value: [5, 20, 36, 10, 20]
+			}
+		}
+	}
 }
 </script>
 
 <style scoped>
-h1 {
-	margin-top: 100px;
+.vote {
+	width: 500px;
+	height: 400px;
 }
 </style>
